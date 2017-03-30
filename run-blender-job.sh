29acc704ceab74242a73b248486d5eb17b7804d3
8bitkick <8bitkick@users.noreply.github.com>
2017-03-30 14:07:02 -0700

    Create run-blender-job.sh

---
+++ run-blender-job.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Blender Render Job
+# ------------------
+# Designed to be run in container instanced by AWS Batch
+# 
+# Parameters: [start frame] [end frame]
+#
+# Environment variables (paths to Amazon S3 files)
+#
+# BLENDER_S3_URL        - source .blend file 
+# FRAME_S3_URL          - destination folder to upload rendered frames to
+#
+#
+
+COUNTER=$1
+
+echo "Blender Render Job"
+echo "------------------"
+echo "jobId: $AWS_BATCH_JOB_ID"
+echo "jobQueue: $AWS_BATCH_JQ_NAME"
+echo "computeEnvironment: $AWS_BATCH_CE_NAME"
+
+# Copy target blender file from s3
+aws s3 cp $BLENDER_S3_URL .
+
+mkdir frames
+
+# render frames from parameters $1 to $2 and copy into s3
+while [ $COUNTER -le $2 ]; do
+        blender -b *.blend -o frames/ -F PNG -E CYCLES -f $COUNTER
+        aws s3 cp frames/  $FRAME_S3_URL --recursive
+        rm frames/*
+        let COUNTER=COUNTER+1
+done
